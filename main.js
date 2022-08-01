dc8b5da801feae4d6c174eb34edb238a9a5d95a4
Aleazar yilma <yilma4060@gmail.com>
2022-08-01 20:04:04 +0300

    fix Delete books

--- main.js
+++ main.js
@@ -1,45 +1,49 @@
 let bookList = new Array();
-getBooks();
+getBooks()
 displayBooks();
-const form = document.querySelector('.addBook');
-const titleInput = document.querySelector('#title');
-const authorInput = document.querySelector('#author');
+const form = document.querySelector(".addBook");
+const titleInput = document.querySelector("#title");
+const authorInput = document.querySelector("#author");
 const deleteBook = document.querySelectorAll('.deleteBook');
-form.addEventListener('submit', addBook);
-deleteBook.forEach((element) => {
-  element.addEventListener('click', function () {
-    var title = this.attr('data-title');
-    console.log(title);
-  });
+form.addEventListener("submit", addBook);
+deleteBook.forEach(element => {
+    element.addEventListener('click', function(){
+        var title= this.dataset.title;
+        var remove =bookList.filter(b=>b.title !== title);
+        localStorage.setItem("books", JSON.stringify(remove));
+       location.reload();
+       
+    });
 });
-function addBook() {
+function addBook(e) {
   let book = {
     title: titleInput.value,
     author: authorInput.value,
   };
   bookList.push(book);
-  localStorage.setItem('books', JSON.stringify(bookList));
+  localStorage.setItem("books", JSON.stringify(bookList));
+  e.preventDefault();
+  location.reload();
 }
 function getBooks() {
-  var books = JSON.parse(localStorage.getItem('books'));
+  var books = JSON.parse(localStorage.getItem("books"));
   if (books !== null) {
     bookList = books;
   }
 }
 function displayBooks() {
-  const bookElement = document.querySelector('#books');
+    const bookElement = document.querySelector("#books");
   if (bookList.length !== 0) {
-    booksHtml = '';
+    booksHtml = "";
     for (const book of bookList) {
       booksHtml +=
-        '<p>' +
+        "<p>" +
         book.title +
-        '</p> <p>' +
+        "</p> <p>" +
         book.author +
-        '</p><button data-title=' +
-        book.title +
-        ' class="deleteBook">Remove</button> <hr>';
+        '</p><button data-title='+book.title+' class="deleteBook">Remove</button> <hr>';
     }
-    bookElement.innerHTML = booksHtml;
+    bookElement.innerHTML = booksHtml; 
+   
   }
 }
